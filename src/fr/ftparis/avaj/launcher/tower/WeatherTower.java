c52bf7e06b467454b269990d25f02a77e9c2bb97
aguiot-- <aguiot--@student.42.fr>
2022-03-07 19:01:19 +0100

    Add basic Tower/WeatherTower classes and Flyable interface

---
+++ src/fr/ftparis/avaj/launcher/tower/WeatherTower.java
@@ -0,0 +1,14 @@
+package fr.ftparis.avaj.launcher.tower;
+
+import fr.ftparis.avaj.launcher.aircraft.Coordinates;
+
+public class WeatherTower extends Tower {
+    public String getWeather(Coordinates coordinates) {
+
+        return ("");
+    }
+
+    private void changeWeather() {
+
+    }
+}
