43c13f718f2480acd3b2b8ff5b4c062e8388a26e
aguiot-- <aguiot--@student.42.fr>
2022-03-07 19:53:22 +0100

    Add AircraftFactory class

---
+++ src/fr/ftparis/avaj/launcher/aircraft/AircraftFactory.java
@@ -0,0 +1,18 @@
+package fr.ftparis.avaj.launcher.aircraft;
+
+import fr.ftparis.avaj.launcher.Flyable;
+import fr.ftparis.avaj.launcher.aircraft.aircrafts.Baloon;
+import fr.ftparis.avaj.launcher.aircraft.aircrafts.Helicopter;
+import fr.ftparis.avaj.launcher.aircraft.aircrafts.JetPlane;
+
+public class AircraftFactory {
+    public Flyable newAircraft(String type, String name, int longitude, int latitude, int height) {
+        Coordinates coordinates = new Coordinates(longitude, latitude, height);
+
+        return switch (type) {
+            case "Baloon" -> new Baloon(name, coordinates);
+            case "Helicopter" -> new Helicopter(name, coordinates);
+            case "JetPlane" -> new JetPlane(name, coordinates);
+        };
+    }
+}
