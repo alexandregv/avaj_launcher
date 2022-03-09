5d2a4d59ecbe54ce1dd508f39431d0dc7a707125
aguiot-- <aguiot--@student.42.fr>
2022-03-09 19:30:56 +0100

    Add very basic weather algorithm

--- src/fr/ftparis/avaj/launcher/WeatherProvider.java
+++ src/fr/ftparis/avaj/launcher/WeatherProvider.java
@@ -15,7 +15,15 @@ public class WeatherProvider {
     }
 
     public String getCurrentWeather(Coordinates coordinates) {
+        long sum = (long) coordinates.getLongitude() + coordinates.getLongitude() + coordinates.getHeight();
 
-        return ("");
+        if (sum % 2 == 0)
+            return weather[0];
+        else if (sum % 3 == 0 || sum % 5 == 0)
+            return weather[1];
+        else if (sum % 7 == 0 || sum % 9 == 0)
+            return weather[2];
+        else
+            return weather[3];
     }
 }
