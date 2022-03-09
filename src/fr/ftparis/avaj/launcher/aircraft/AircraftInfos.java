7d0d9d5ae83562b1a8521d2123509b82b4fd6aaa
aguiot-- <aguiot--@student.42.fr>
2022-03-09 18:12:33 +0100

    Remove useless getters in AircraftInfos record

--- src/fr/ftparis/avaj/launcher/aircraft/AircraftInfos.java
+++ src/fr/ftparis/avaj/launcher/aircraft/AircraftInfos.java
@@ -1,27 +1,6 @@
 package fr.ftparis.avaj.launcher.aircraft;
 
 public record AircraftInfos(String type, String name, int longitude, int latitude, int height) {
-
-    public String getType() {
-        return type;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getLongitude() {
-        return longitude;
-    }
-
-    public int getLatitude() {
-        return latitude;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
     public String toString() {
         return "TYPE: " + this.type + "\nNAME: " + this.name + "\nLONGITUDE: " + this.longitude + "\nLATITUDE: " + this.latitude + "\nHEIGHT: " + height + "\n";
     }
